b9f7fa4226bad7d260b12aad1d1eed6a6979d1a2
NGUYEN <darkevil9712@gmail.com>
2017-12-03 01:49:58 +0700

    update

---
+++ DuAnMoiEJB/ejbModule/test_case/testCSDL.java
@@ -0,0 +1,14 @@
+package test_case;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testCSDL {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
